928e40e971aa0306fd0a19a482164277cf15a333
Michelle Wong <michellekwong2@gmail.com>
2021-03-08 15:42:23 -0500

    HTML form layout

--- form/src/app/app.module.ts
+++ form/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormComponent } from './form/reactive-form/reactive-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReactiveFormComponent
   ],
   imports: [
     BrowserModule,
